65d9e35e14623c1079ac87e8411ca2d0e5cf4727
Jan Gru <j4n6ru@gmail.com>
2022-03-06 17:03:26 +0100

    Modify org-roam-node-read to use consult
    Modify org-roam-node-read to use consult and introduce all necessary
    helper functions

--- org-roam-consult.el
+++ org-roam-consult.el
@@ -36,7 +36,7 @@ supplied. Can take a PROMPT argument."
       :state (consult--file-preview))))
 
 (defun org-roam-consult--ids-to-files (ids)
-  "Take a bunch of IDS of org-roam and convert those into file paths."
+  "Take a bunch of IDS of org-roam-nodes and convert those into file paths."
   (mapcar #'(lambda (id)
               (org-roam-node-file (org-roam-node-from-id (car id))))
          ids))
@@ -59,6 +59,95 @@ supplied. Can take a PROMPT argument."
   "Find org-roam node with preview."
   (interactive "")
   (find-file (org-roam-consult--select-file "Node: ")))
+
+;; Completing-read interface using consult
+;; Override `org-roam-node-read' so that each an every completing function resorts to consult
+;; https://github.com/org-roam/org-roam/blob/f50d6e7376b3ba603fb4df1d95204ca5f3cc3ca8/org-roam-node.el#L510
+(defun org-roam-node-read (&optional initial-input filter-fn sort-fn require-match prompt)
+  "Read and return an `org-roam-node' with the help of consult.
+INITIAL-INPUT is the initial minibuffer prompt value.
+FILTER-FN is a function to filter out nodes: it takes an `org-roam-node',
+and when nil is returned the node will be filtered out.
+SORT-FN is a function to sort nodes. See `org-roam-node-read-sort-by-file-mtime'
+for an example sort function.
+If REQUIRE-MATCH, the minibuffer prompt will require a match.
+PROMPT is a string to show at the beginning of the mini-buffer, defaulting to \"Node: \""
+  (let* ((nodes (org-roam-node-read--completions filter-fn sort-fn))
+         (prompt (or prompt "Node: "))
+         (node
+          (consult--read
+              nodes
+              :prompt prompt
+              :initial initial-input
+              :predicate filter-fn
+              :sort sort-fn
+              :require-match t
+              :category 'org-roam-node
+              :annotate (lambda (title)
+                              (funcall org-roam-node-annotation-function
+                                       (get-text-property 0 'node title)))
+              :history org-roam-node-history
+              :state (org-roam-consult--node-preview)
+              :preview-key 'any)))
+    (or (cdr (assoc node nodes))
+        (org-roam-node-create :title node))))
+
+;; Minimally adapted from https://github.com/minad/consult/blob/8547e336142e74449b59a6b018e3c96a2b205fd2/consult.el#L1103
+(defun org-roam-consult--temporary-nodes ()
+  "Return a function to open nodes temporarily."
+  (let* ((new-buffers)
+         (old-buffers (buffer-list))
+         (dir default-directory))
+    (lambda (&optional node)
+      (if node
+          (let ((default-directory dir)
+                (inhibit-message t)
+                (enable-dir-local-variables nil)
+                (enable-local-variables (and enable-local-variables :safe))
+                (non-essential t)
+                (name
+                 (org-roam-node-file (get-text-property 0 'node node))))
+            (or
+             ;; get-file-buffer is only a small optimization here. It
+             ;; may not find the actual buffer, for directories it
+             ;; returns nil instead of returning the Dired buffer.
+             (get-file-buffer name)
+             ;; file-attributes may throw permission denied error
+             (when-let* ((attrs (ignore-errors (file-attributes name)))
+                         (size (file-attribute-size attrs)))
+               (if (> size consult-preview-max-size)
+                      (prog1 nil
+                        (message "File `%s' (%s) is too large for preview"
+                                 name (file-size-human-readable size)))
+                 (cl-letf* (((default-value 'find-file-hook)
+                             (seq-remove (lambda (x)
+                                           (memq x consult-preview-excluded-hooks))
+                                         (default-value 'find-file-hook)))
+                            (buf (find-file-noselect
+                                  name 'nowarn
+                                  (> size consult-preview-raw-size))))
+                   ;; Only add new buffer if not already in the list
+                   (unless (or (memq buf new-buffers) (memq buf old-buffers))
+                     (push buf new-buffers)
+                     ;; Only keep a few buffers alive
+                     (while (> (length new-buffers) consult-preview-max-count)
+                       (consult--kill-clean-buffer (car (last new-buffers)))
+                       (setq new-buffers (nbutlast new-buffers))))
+                   buf)))))
+        (mapc #'consult--kill-clean-buffer new-buffers)))))
+
+;; Taken from https://github.com/minad/consult/blob/8547e336142e74449b59a6b018e3c96a2b205fd2/consult.el#L3265
+;; and adapted to use `org-roam-consult--temporary-nodes'
+(defun org-roam-consult--node-preview ()
+  "Create preview function for nodes."
+  (let ((open (org-roam-consult--temporary-nodes))
+        (preview (consult--buffer-preview)))
+    (lambda (cand restore)
+      (if restore
+          (progn
+            (funcall preview nil t)
+            (funcall open))
+        (funcall preview (and cand (funcall open cand)) nil)))))
 
 (provide 'org-roam-consult)
 ;;; org-roam-consult.el ends here
