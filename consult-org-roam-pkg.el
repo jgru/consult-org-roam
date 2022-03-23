5aaf6296470f5dc586e684c70408ca4ac734abb9
jgru <j4n6ru@gmail.com>
2022-03-23 09:07:19 +0100

    Add pgk file

---
+++ consult-org-roam-pkg.el
@@ -0,0 +1,6 @@
+(define-package "consult-org-roam" "0.6.2"
+  "Operate Org Roam with the help of Consult"
+  '((emacs "27.1")
+    (org-roam "2.2.0")
+    (consult "0.16"))
+  :homepage "https://github.com/jgru/consult-org-roam")
